a6bc4b1d535ef8f996ab49148c90cc2f488472bc
nakiwuge <nakiwuge@gmail.com>
2019-11-22 12:56:40 +0300

    update readMe

--- data.js
+++ data.js
@@ -1,184 +1,184 @@
 module.exports = [
     {
         name: "albert",
-        geometry: {
+        location: {
             lat: 1.852996,
             long: 31.000378
         }
     },
     {
         name: "bisina",
-        geometry: {
+        location: {
             lat:1.638376,
             long:33.954202
         }
     },
     {
         name: "bugondo",
-        geometry:{
+        location:{
             lat:null,
             long:null
         }
     },
     {
         name: "buhera",
-        geometry:{
+        location:{
             lat:null,
             long:null
         }
     },
     {
         name: "bujuku",
-        geometry:{
+        location:{
             lat:null,
             long:null
         }
     },
     {
         name: "bunyonyi",
-        geometry:{
+        location:{
             lat:-1.283944,
             long:29.913834,
         }
     },
     {
         name: "edward",
-        geometry:{
+        location:{
             lat:-0.278037,
             long:29.696589,
         }
     },
     {
         name: "kabaka",
-        geometry:{
+        location:{
             lat: 0.297497,
             long:32.561672,
         }
     },
     {
         name: "kachera",
-        geometry:{
+        location:{
             lat: -0.549863,
             long:31.094466,
         }
     },
     {
         name: "katwe",
-        geometry:{
+        location:{
             lat:-0.126972,
             long:29.867051,
         }
     },
     {
         name: "kayumbu",
-        geometry:{
+        location:{
             lat: -1.336563,
             long:29.787626,
         }
     },
     {
         name: "kitandra",
-        geometry:{
+        location:{
             lat: null,
             long:null,
         }
     },
     {
         name: "kwania",
-        geometry:{
+        location:{
             lat: 1.772074,
             long:32.737078,
         }
     },
     {
         name: "kyahafi",
-        geometry:{
+        location:{
             lat: null,
             long:null,
         }
     },
     {   name: "kyoga",
-        geometry:{
+        location:{
             lat: 1.537414,
             long: 32.946097,
         }
     },
     {
         name: "mburo",
-        geometry:{
+        location:{
             lat: -0.668101,
             long: 30.924603,
        }
     },
     {
         name: "mutanda",
-        geometry:{
+        location:{
             lat:-1.202227,
             long: 29.680209,
         }
     },
     {
         name: "mulehe",
-        geometry:{
+        location:{
             lat:-1.216301,
             long:  29.722223,
         }
     },
     {   name: "nabugabo",
-        geometry:{
+        location:{
             lat: -0.355508,
             long: 31.913181,
         }
     },
     {
         name: "nakuwa",
-        geometry:{
+        location:{
             lat: 1.177552,
             long: 33.464998,
         }
     },
     {
         name: "nkugute",
-        geometry:{
+        location:{
             lat: -0.315079,
             long: 30.099871,
         }
     },
     {
         name: "nyabihoko",
-        geometry:{
+        location:{
             lat: -0.904417,
             long: 30.121400,
         }
     },
     {   name: "nyamusingire",
-        geometry:{
+        location:{
             lat: -0.298584,
             long: 30.033333,
         }
     },
     {   name: "opeta",
-        geometry:{
+        location:{
             lat: 1.648486,
             long: 34.156124,
         }
     },
     {   name: "saka",
-        geometry:{
+        location:{
             lat: 0.701150,
             long: 30.233347,
         }
     },
     {
         name: "victoria",
-        geometry:{
+        location:{
             lat: -0.595139,
             long: 33.400850,
         }
     },
     {
         name: "wamala",
-        geometry:{
+        location:{
             lat: 0.353049,
             long: 31.890252
         }
